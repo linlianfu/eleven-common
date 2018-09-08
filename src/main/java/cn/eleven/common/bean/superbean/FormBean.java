d32454c97cfb401f84d2197601d406c3b7d3bbcb
linlianfu <1241352445@qq.com>
2018-09-08 19:39:28 +0800

    提交超级bean

---
+++ src/main/java/cn/eleven/common/bean/superbean/FormBean.java
@@ -0,0 +1,11 @@
+package cn.eleven.common.bean.superbean;
+
+import java.io.Serializable;
+
+/**
+ * @author: eleven
+ * @date: 2018/8/21 21:51
+ * @description: 表征定义的bean是接收前端提交的表单数据
+ */
+public interface FormBean extends Serializable {
+}
